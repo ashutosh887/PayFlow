ef4357e2ef03d2f9d8d7de52c2901710cdb0c17a
Ashutosh Jha <ashutoshj887@gmail.com>
2026-01-10 00:57:52 +0530

    feat: integrate theme management

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,9 @@ import type { Metadata } from "next";
 import "./globals.css";
 import config from "@/config";
 import { Nunito_Sans } from "next/font/google";
+import { ThemeProvider } from "@/components/providers/ThemeProvider";
 
-const nunitoSans = Nunito_Sans({variable:'--font-sans'});
+const nunitoSans = Nunito_Sans({ variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: config.appName,
@@ -12,15 +13,24 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en" className={nunitoSans.variable}>
-      <body
-        className="antialiased"
-      >
-        {children}
+    <html
+      lang="en"
+      className={nunitoSans.variable}
+      suppressHydrationWarning
+    >
+      <body className="antialiased">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem={false}
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
